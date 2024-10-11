840f799bfa6901be278899503619df6bab306a06
Rishi Sharma <113165973+rdsharma10@users.noreply.github.com>
2024-10-11 23:21:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-const data=require("./data.json")
-console.log(data.name
-    )
+let arr=[1010101,101010,1010101,1010101];
+let ans=arr.map((x)=>{
+  return x+"0b";
+}).join(" ")
+let ans2=[...ans].join("").toString(10);
+ans2.map((x)=>(x.replace(x[0],x[-1])))
+console.log(ans2)
