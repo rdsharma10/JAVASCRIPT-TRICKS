a4ff864d0b196ded9d5d90f2be215ce27cc09520
Rishi Sharma <rdsharma841@gmail.com>
2026-02-25 14:40:47 +0530

    Adding

--- script9.js
+++
@@ -1,9 +0,0 @@
-function handlingConfirm()
-{
-    var x= confirm("Do you want to see the marvel ");
-    if(x){
-        console.log("thor","ironman","spiderman");
-    }
-    console.log("thank you for visiting");
-    console.log("have a nice day");
-}
