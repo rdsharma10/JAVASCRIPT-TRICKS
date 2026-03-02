b8a5c126698f7ad7af0d0a3a02f2497ce16c5b7f
Rishi Sharma <rdsharma841@gmail.com>
2026-03-02 16:58:55 +0530

    Adding new logic

--- reversesentence.js
+++ reversesentence.js
@@ -2,8 +2,25 @@ let str="My name is Rishi";
 let ans="";
 for(let i=str.length-1;i>=0;i--){
     if(str[i]==" "){
-        ans+=str[i];
+         ans+=str[i];
     }
 }
 console.log(ans);
 console.log(ans.length);
+console.log(ans.length-1);
+let j=ans.length-1;                     
+
+for(let i=str.length-1;i>=0;i--){   
+    if(str[i]!=" "){    
+        ans+=str[i];    
+    }
+    else{
+        while(j>=0 && ans[j]==" "){
+            j--;
+        }
+        while(j>=0 && ans[j]!=" "){
+            ans+=ans[j];
+            j--;
+        }
+    }    
+}       
