e6d8086c313379b9df3d18a63e699cfb60b0c4d0
Rishi Sharma <rdsharma841@gmail.com>
2026-03-02 12:52:49 +0530

    New Branch

--- reversesentence.js
+++ reversesentence.js
@@ -2,8 +2,8 @@ let str="My name is Rishi";
 let ans="";
 for(let i=str.length-1;i>=0;i--){
     if(str[i]==" "){
-        ans+=str[i];
-        
+        ans+=str[i];ans+=str.slice(i+1);
+        str=str.slice(0,i);  
     }
 }
 console.log(ans);
